dd78241a16bbc251a2d15f07178dc55ec98b6c82
Eric <eric.ou@yale.edu>
2020-07-28 00:49:08 -0700

    sharing backend complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to :user
-  has_many :sections
-  has_many :user_shared_topics, foreign_key: "shared_topic_id"
+  has_many :sections, dependent: :destroy
+  has_many :user_shared_topics, foreign_key: "shared_topic_id", dependent: :destroy
   validates :title, {presence: true}
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :topics
-    has_many :user_shared_topics, foreign_key: "sharee_id"
-    has_many :shared_topics, through: :user_shared_topics
+    has_many :topics, dependent: :destroy
+    has_many :user_shared_topics, foreign_key: "sharee_id", dependent: :destroy
+    has_many :shared_topics, through: :user_shared_topics, dependent: :destroy
 
     has_secure_password
     validates :username, presence:true 
